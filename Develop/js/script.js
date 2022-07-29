34f4a7167aa4c54a74cbafca5a25e6ab02a6fa06
Greg <litm47@gmail.com>
2022-07-29 14:37:33 -0400

    added color and download button functionality

---
+++ Develop/js/script.js
@@ -0,0 +1,39 @@
+
+
+var box = document.getElementById("box")
+
+var whiteBtn = document.getElementById("white")
+var blackBtn = document.getElementById("black")
+var randomBtn = document.getElementById("random")
+var down = document.getElementById("down")
+
+whiteBtn.addEventListener("click", function(){
+    box.style.backgroundColor = "white"
+})
+
+blackBtn.addEventListener("click", function(){
+    box.style.backgroundColor = "black"
+})
+
+randomBtn.addEventListener("click", randomColor)
+
+down.addEventListener("click", downloading)
+
+
+function randomColor(){
+var randomCss = Math.floor(Math.random()*1677215).toString(16);
+box.style.backgroundColor = "#" + randomCss;
+
+}
+
+function downloading(){
+html2canvas(box).then(function(canvas){
+    
+const image = canvas.toDataURL("image/png", 1.0)
+    const link = document.createElement("a")
+    link.download = "profile-picture.png";
+    link.href = image;
+    link.click()
+})
+   
+}
