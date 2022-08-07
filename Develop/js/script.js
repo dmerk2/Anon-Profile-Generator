11d62a89598fc8049776f4a517cbe928a10113db
ssavane26 <savaneprod@gmail.com>
2022-08-06 20:22:12 -0400

    added comments to script.js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -11,7 +11,7 @@ var reRoll = document.getElementById("yesroll");
 var clip = document.getElementById("copy");
 var save = document.getElementById("save");
 
-// gets from pixel api
+// gets monster svg from pixel api
 function getMonster() {
   var requestUrl =
     "	https://fast-dawn-89938.herokuapp.com/https://app.pixelencounter.com/api/basic/monsters/random";
@@ -28,7 +28,7 @@ function getMonster() {
   xhr.send();
 }
 
-// gets from fact api
+// gets random fact from fact api
 function getFact() {
   var requestUrl = "https://uselessfacts.jsph.pl/random.json?language=en";
 
@@ -41,13 +41,13 @@ function getFact() {
     });
 }
 
-// random css color
+// random css color generation
 function randomColor() {
   var randomCss = Math.floor(Math.random() * 1677215).toString(16);
   box.style.backgroundColor = "#" + randomCss;
 }
 
-// downloads box div as a png
+// downloads profile picture render as a png
 function downloading() {
   html2canvas(box).then(function (canvas) {
     const image = canvas.toDataURL("image/png", 1.0);
@@ -74,34 +74,39 @@ function copyFact() {
 }
 
 
-
+// calls apis for initiation
 function run() {
   getFact();
   getMonster();
 }
 
 
-
+//changes background color to white
 whiteBtn.addEventListener("click", function () {
   box.style.backgroundColor = "white";
 });
 
-
+//changes background color to black
 blackBtn.addEventListener("click", function () {
   box.style.backgroundColor = "black";
 });
 
+//changes background color to random css color
 randomBtn.addEventListener("click", randomColor);
 
+// triggers download of profile picture
 down.addEventListener("click", downloading);
 
+//triggers regeneration of apis
 reRoll.addEventListener("click", run);
 
+// triggers bio clipboard copy
 clip.addEventListener("click", copyFact);
 
 
 run();
 
+//stores profile picture as data url in local storage
 function storeProfile() {
   html2canvas(box).then(function (canvas) {
     const image = canvas.toDataURL("image/png", 1.0);
@@ -109,6 +114,7 @@ function storeProfile() {
   });
 }
 
+// sets local storage object of saved profile picture and bio
 function saveLastProfile(pic) {
   var savedProfile = JSON.parse(localStorage.getItem("allProfiles"));
   if (savedProfile === null) savedProfile = [];
@@ -122,7 +128,7 @@ function saveLastProfile(pic) {
   localStorage.setItem("allProfiles", JSON.stringify(savedProfile));
 }
 
-
+//triggers profile picture and bio storage functions
 save.addEventListener("click", function () {
   storeProfile();
 });
